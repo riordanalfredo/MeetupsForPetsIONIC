8f3a59456fd3def609790335c7fd65fa80aa79d7
Mubtasim Mahmud <mubtasimmahmud20@gmail.com>
2019-08-17 22:58:58 +1000

    Demo: Add event to calendar
    Note: needed to use v4 of cordova-plugin-calendar and corresponding npm package instead of default (v5)

--- PetsMeetupsV3/src/pages/home/home.ts
+++ PetsMeetupsV3/src/pages/home/home.ts
@@ -1,14 +1,19 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
+import { Calendar } from '@ionic-native/calendar';
+
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
-
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController, private calendar: Calendar) {
 
   }
 
+  createEvent() {
+    this.calendar.createEvent('Pet Meetup', 'My place', 'Bring pets', new Date(), new Date())
+      .then(msg => console.log(msg), err => console.log(err));
+  }
 }
