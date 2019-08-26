c88ab9ae41ef7e2bcc2bccf4467c2da585bf0600
rqmok <rqmok@hotmail.com>
2019-08-26 11:09:03 +1000

    Pet: Store ID for a pet. Implement the constructor.

--- PetsMeetupsV3/src/models/Pet.ts
+++ PetsMeetupsV3/src/models/Pet.ts
@@ -1,9 +1,18 @@
 export class Pet {
+    private id: string;
     private name: string;
     private description: string;
     private avatarUrl: string;
 
-    constructor(name: string, description: string, avatarUrl: string) {
+    constructor(id: string, name: string, description: string, avatarUrl: string) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.avatarUrl = avatarUrl;
+    }
+
+    public getId(): string {
+        return this.id;
     }
 
     public getName(): string {
