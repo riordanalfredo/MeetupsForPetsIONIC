5c10b0c2d07335921d86fe6039376283c4a824ab
Mubtasim Mahmud <mubtasimmahmud20@gmail.com>
2019-10-10 15:48:57 +1100

    Added comments and cleaned up code

--- parse_ionic/src/main/java/Parser.java
+++ parse_ionic/src/main/java/Parser.java
@@ -14,19 +14,21 @@ public class Parser {
     public static void main(String[] args) {
         projectPath = "D:\\MeetupsForPetsIONIC-master\\PetsMeetupsV3";
         filePath = "src\\pages\\add-pet\\add-pet.html";
+        // Get a file handle for the html file
         File input = new File(projectPath + "\\" + filePath);
-
+        // Only parse if file is html
         if (input.isFile() && input.getName().endsWith(".html")) {
             parseButtons(input);
         }
     }
 
     private static void parseButtons(File input) {
+        // DOM structure of the file
         Document doc;
 
         try{
             doc = Jsoup.parse(input, "UTF-8", "");
-
+            // Select all buttons
             Elements buttons = doc.select("button");
 
             for (Element button : buttons){
@@ -34,8 +36,13 @@ public class Parser {
                 System.out.println(button.toString());
                 // Ionic 4 button
                 Element v4Button = new Element("ion-button");
+                // Loop through all attributes of v3 button
                 for (Attribute attribute : button.attributes()) {
+                    // We only want to change ion-buttons, not regular ones
                     if (!attribute.toString().equals("ion-button")) {
+                        // Change attributes from v3 to v4 according to docs
+                        // Any that don't change can just have the old key and value
+                        // from the v3 button
                         if (attribute.toString().equals("clear")) {
                             v4Button.attr("fill", "clear");
                         } else {
@@ -44,15 +51,12 @@ public class Parser {
                     }
                 }
                 System.out.println(v4Button.toString() + "\n");
+                // Replace the old button with the new button in the DOM structure
                 button.replaceWith(v4Button);
             }
-
+            // The new DOM structure can be given to the model
+            // to be generated using Acceleo
             System.out.println(doc.toString());
-
-//            FileOutputStream is = new FileOutputStream(input);
-//            OutputStreamWriter osw = new OutputStreamWriter(is);
-//            Writer w = new BufferedWriter(osw);
-//            w.write(doc.toString());
 
         } catch (IOException e) {
             e.printStackTrace();
