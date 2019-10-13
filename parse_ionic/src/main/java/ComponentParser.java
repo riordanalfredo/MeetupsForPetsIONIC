3a00791593ea2cc83ebb26cd83cef1a13f6921c8
Raymond Nguyen <rmngu2@student.monash.edu>
2019-10-13 14:34:49 +1100

    Parser for IonicPage
    - Parser to remove redundant Ionic 3 IonicPage annotation from a typescript file

--- parse_ionic/src/main/java/ComponentParser.java
+++ parse_ionic/src/main/java/ComponentParser.java
@@ -6,12 +6,16 @@ public class ComponentParser {
 
     // For testing purposes
     public static void main(String[] args) {
+
         String projectPath = "C:\\Users\\rbngu\\OneDrive\\Desktop\\MeetupsForPetsIONIC\\PetsMeetupsV3";
         String filePath = "\\src\\pages\\add-pet\\add-pet.ts";
 
         ComponentParser testParser = new ComponentParser();
+
         System.out.println(testParser.migrateIonViewDidLoad(projectPath, filePath));
+        System.out.println(testParser.migrateIonicPageAnnotation(projectPath, filePath));
     }
+
     public String migrateIonViewDidLoad(String projectPath, String filePath){
         String newVersion = "";
         try{
@@ -23,7 +27,21 @@ public class ComponentParser {
         }catch(IOException e){
             System.out.println(e);
         }
-
         return "";
     }
+
+    public String migrateIonicPageAnnotation(String projectPath, String filePath){
+        String newVersion = "";
+        try{
+            newVersion = new String(Files.readAllBytes(Paths.get(projectPath + filePath)));
+            if(newVersion.contains("@IonicPage()")){
+                newVersion = newVersion.replaceAll("@IonicPage\\(\\)","");
+            }
+            return newVersion;
+        }catch(IOException e){
+            System.out.println(e);
+        }
+        return "";
+    }
+
 }
