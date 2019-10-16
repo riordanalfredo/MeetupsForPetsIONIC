1dbbb7baf7199689a5eb7825316e3f8953fad65d
Raymond Nguyen <rmngu2@student.monash.edu>
2019-10-16 21:10:30 +1100

    Migrating @Injectable
    - Migrated @Injectable annotation from Ionic 3 to Ionic 4 (now inserts 'providedIn: 'root'' value)

--- parse_ionic/src/main/java/ComponentParser.java
+++ parse_ionic/src/main/java/ComponentParser.java
@@ -9,12 +9,13 @@ public class ComponentParser {
 
         String projectPath = "C:\\Users\\rbngu\\OneDrive\\Desktop\\MeetupsForPetsIONIC\\PetsMeetupsV3";
         String filePath = "\\src\\pages\\add-pet\\add-pet.ts";
-
+        String servicePath = "\\src\\providers\\db\\db.ts";
         ComponentParser testParser = new ComponentParser();
 
-        System.out.println(testParser.migrateIonViewDidLoad(projectPath, filePath));
-        System.out.println(testParser.migrateIonicPageAnnotation(projectPath, filePath));
-        System.out.println(testParser.migrateIonicAngularImports(projectPath, filePath));
+        //System.out.println(testParser.migrateIonViewDidLoad(projectPath, filePath));
+        //System.out.println(testParser.migrateIonicPageAnnotation(projectPath, filePath));
+        //System.out.println(testParser.migrateIonicAngularImports(projectPath, filePath));
+        System.out.println(testParser.migrateInjectable(projectPath, servicePath));
     }
 
     public String migrateIonViewDidLoad(String projectPath, String filePath){
@@ -52,6 +53,23 @@ public class ComponentParser {
             newVersion = new String(Files.readAllBytes(Paths.get(projectPath + filePath)));
             if(newVersion.contains("ionic-angular")){
                 newVersion = newVersion.replaceAll("ionic-angular","@ionic/angular");
+            }
+            return newVersion;
+        }catch(IOException e){
+            System.out.println(e);
+        }
+        return "";
+    }
+
+    public String migrateInjectable(String projectPath, String filePath){
+        try{
+            String newVersion = new String(Files.readAllBytes(Paths.get(projectPath + filePath)));
+            if(newVersion.contains("@Injectable()")){
+                // Finds where the '(' starts in the Injectable annotation
+                int startingBracket = newVersion.indexOf("@Injectable()") + 12;
+
+                // Make the change to have the providedIn value
+                newVersion = newVersion.substring(0, startingBracket) + "{\n\t providedIn: 'root'\n}" + newVersion.substring(startingBracket);
             }
             return newVersion;
         }catch(IOException e){
